7c1afbfe8a85534ed5a66065d14e6448d7e2db6b
momocntt <nguyenthihoaimo1207@gmail.com>
2024-11-18 11:28:22 +0700

    Add project files.

---
+++ Bai14_GiaiThua.cpp
@@ -0,0 +1,19 @@
+// Bai14_GiaiThua
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "TINH GIAI THUA\n" << endl;
+	int so, giaithua = 1;
+	cout << "nhap so de tinh giai thua:";
+	cin >> so;
+	for (int i = 1; i <= so; i++)
+	{
+		giaithua *= i;
+	}
+	cout << so << "! =" << giaithua;
+	return 0;
+}
+
+
+
